599bb8741a83dcb2106c7b26b1ad97b61a1406aa
Annehelen-ltda <71146646+Annehelen-ltda@users.noreply.github.com>
2020-09-26 16:23:50 -0300

    Type S. 6 Pass

--- cupom.test.ts
+++ cupom.test.ts
@@ -92,25 +92,25 @@ test('Loja Completa', () => {
 test('Nome vazio', () => {
   cupom.dados.nome_loja = "";
   verificaCampoObrigatorio(`O campo nome da loja é obrigatório`);
-  cupom.dados.nome_loja = "Arcos Dourados Com. de Alimentos LTDA";
+  cupom.dados.nome_loja = "Loja 1";
 });
 
 test('Logradouro vazio', () => {
   cupom.dados.logradouro = "";
   verificaCampoObrigatorio(`O campo logradouro do endereço é obrigatório`);
-  cupom.dados.logradouro = "Av. Projetada Leste";
+  cupom.dados.logradouro = "Log 1";
 });
 
 test('Número zero', () => {
   cupom.dados.numero = 0;
   expect(cupom.dados_loja()).toBe(TEXTO_ESPERADO_SEM_NUMERO);
-  cupom.dados.numero = 500;
+  cupom.dados.numero = 10;
 });
 
 test('Complemento vazio', () => {
   cupom.dados.complemento = "";
   expect(cupom.dados_loja()).toBe(TEXTO_ESPERADO_SEM_COMPLEMENTO);
-  cupom.dados.numero = "C1";
+  cupom.dados.complemento = "C1";
 });
 
 test('Bairro vazio', () => {
@@ -122,13 +122,13 @@ test('Bairro vazio', () => {
 test('Município vazio', () => {
   cupom.dados.municipio = "";
   verificaCampoObrigatorio(`O campo município do endereço é obrigatório`);
-  cupom.dados.municipio = "Campinas";
+  cupom.dados.municipio = "Mun 1";
 });
 
 test('Estado vazio', () => {
   cupom.dados.estado = "";
   verificaCampoObrigatorio(`O campo estado do endereço é obrigatório`);
-  cupom.dados.estado = "SP";
+  cupom.dados.estado = "E1";
 });
 
 test('CEP vazio', () => {
@@ -152,33 +152,39 @@ test('Observação vazia', () => {
 test('CNPJ vazio', () => {
   cupom.dados.cnpj = "";
   verificaCampoObrigatorio(`O campo CNPJ da loja é obrigatório`);
-  cupom.dados.cnpj = "42.591.651/0797-34";
+  cupom.dados.cnpj = "11.111.111/1111-11";
 });
 
 test('Inscrição estadual vazia', () => {
   cupom.dados.inscricao_estadual = "";
   verificaCampoObrigatorio(`O campo inscrição estadual da loja é obrigatório`);
-  cupom.dados.inscricao_estadual = "244.898.500.113";
+  cupom.dados.inscricao_estadual = "123456789";
 });
 
 test('Exercício 2 - customizado', () => {
 
   // Defina seus próprios valores para as variáveis a seguir
-  cupom.dados.nome_loja = "";
-  cupom.dados.logradouro = "";
-  cupom.dados.numero = 0;
-  cupom.dados.complemento = "";
-  cupom.dados.bairro = "";
-  cupom.dados.municipio = "";
-  cupom.dados.estado = "";
-  cupom.dados.cep = "";
-  cupom.dados.telefone = "";
-  cupom.dados.observacao = "";
-  cupom.dados.cnpj = "";
-  cupom.dados.inscricao_estadual = "";
+  cupom.dados.nome_loja = "Smelly Cat";
+  cupom.dados.logradouro = "Rua Etheria";
+  cupom.dados.numero = 205;
+  cupom.dados.complemento = "Perto da velhinha que mora em uma caverna";
+  cupom.dados.bairro = "Br. Templo do Cristal";
+  cupom.dados.municipio = "Beach City";
+  cupom.dados.estado = "BC";
+  cupom.dados.cep = "78051-604";
+  cupom.dados.telefone = "(66)4002-8922";
+  cupom.dados.observacao = "Por Favor ignorar os exército Intergalácticos em guerra tentando dominar o planeta";
+  cupom.dados.cnpj = "53.409.609/0001-85";
+  cupom.dados.inscricao_estadual = "512.670.302.653";
+  
+  let expected = "Smelly Cat\n";
+  expected += "Rua Etheria, 205 Perto da velhinha que mora em uma caverna\n";
+  expected += "Br. Templo do Cristal - Beach City - BC\n";
+  expected += "CEP:78051-604 Tel (66)4002-8922\n";
+  expected += "Por Favor ignorar os exército Intergalácticos em guerra tentando dominar o planeta\n";
+  expected +="CNPJ: 53.409.609/0001-85\n";
+  expected += "IE: 512.670.302.653\n";
 
   //E atualize o texto esperado abaixo
-  expect(cupom.dados_loja()).toBe(
-    `
-`);
+  expect(cupom.dados_loja()).toBe(expected);
 });
--- cupom.ts
+++ cupom.ts
@@ -12,10 +12,70 @@ let dados = {
   cnpj: "42.591.651/0797-34",
   inscricao_estadual: "244.898.500.113"
 };
-
+function isEmpty(str: string): boolean {
+  let spaceCount = str.length - str.replace(" ", "").length;
+  return str == null || spaceCount == str.length;
+}
 function dados_loja() {
   // Implemente aqui
-  return "";
+  if (dados.nome_loja == "") {
+    throw new Error(`O campo nome da loja é obrigatório`);
+  }
+  if(dados.logradouro == ""){
+    throw new Error(`O campo logradouro do endereço é obrigatório`);
+  }
+  let num = dados.numero + ""
+  if(dados.numero == 0){
+    num = "s/n";
+  }
+  if(dados.municipio == ""){
+    throw new Error(`O campo município do endereço é obrigatório`);
+  }
+  if(dados.estado == ""){
+    throw new Error(`O campo estado do endereço é obrigatório`);
+  }
+  if(dados.cnpj == ""){
+    throw new Error(`O campo CNPJ da loja é obrigatório`);
+  }
+  if(dados.inscricao_estadual == ""){
+    throw new Error(`O campo inscrição estadual da loja é obrigatório`);
+  }
+
+  var part2 = `${dados.logradouro}, ${num}`;
+  if (! isEmpty (dados.complemento)){
+      part2 += ` ${dados.complemento}`;
+    }
+  var part3 = "";
+  if (! isEmpty (dados.bairro)){
+      part3 += `${dados.bairro} - `;
+  }
+  part3 += `${dados.municipio} - ${dados.estado}`;
+  
+  var part4 = "";
+  if (! isEmpty (dados.cep)){
+      part4 = `CEP:${dados.cep}`;
+    }
+  if (! isEmpty(dados.telefone)){
+      if (! isEmpty(part4)){
+        part4 += ` `;
+      }
+      part4 += `Tel ${dados.telefone}`;
+    }
+  if (! isEmpty(part4)){
+      part4 += `\n`;
+    }
+  var part5 = "";
+  if (! isEmpty(dados.observacao)){
+      part5 += `${dados.observacao}`;
+    }
+  let output = `${dados.nome_loja}\n`;
+  output += `${part2}\n`;
+  output += `${part3}\n`;
+  output += `${part4}`;
+  output += `${part5}\n`;
+  output += `CNPJ: ${dados.cnpj}\n`;
+  output += `IE: ${dados.inscricao_estadual}\n`;
+  return output;
 }
 
 module.exports = {
